99c864d64535fe9aa67bd414bde275400a5de2e2
sun6r0 <55103268+sun6r0@users.noreply.github.com>
2020-07-29 12:21:38 +0400

    Added UI tests

--- src/main/java/pageObjects/MailPage.java
+++ src/main/java/pageObjects/MailPage.java
@@ -1,12 +1,9 @@
 package pageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
